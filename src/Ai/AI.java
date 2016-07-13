15f3fed75b01709fbfdd62ee14881c195e74a35f
NIKMC-I <nikmc10@mail.ru>
2016-07-14 00:05:15 +0300

    start

---
+++ src/Ai/AI.java
@@ -0,0 +1,8 @@
+package Ai;
+
+/**
+ * Created by NIKMC-I on 14.07.2016.
+ */
+public class AI {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by NIKMC-I on 13.07.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hi");
+    }
+}
