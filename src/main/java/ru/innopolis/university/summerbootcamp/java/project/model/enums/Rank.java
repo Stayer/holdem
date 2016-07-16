ba39d05a1b888a32b60a555b7e1f9f0e34fc01f2
Iskandar <iskandar@mbpro.local>
2016-07-17 00:59:13 +0300

    Tests: checker test init; cards logic fixes #HOLDEM-17

---
+++ src/main/java/ru/innopolis/university/summerbootcamp/java/project/model/enums/Rank.java
@@ -0,0 +1,8 @@
+package ru.innopolis.university.summerbootcamp.java.project.model.enums;
+
+/**
+ * Created by iskandar on 17/07/16.
+ */
+public enum Rank {
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+}
