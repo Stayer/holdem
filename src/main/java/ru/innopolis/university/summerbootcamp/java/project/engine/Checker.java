9b7f13f59548d2ecd03698263f1a1f800ae95cff
Stayer <rodionov12@gmail.com>
2016-07-18 19:31:25 +0600

    Update Checker. #holdem-15

--- src/main/java/ru/innopolis/university/summerbootcamp/java/project/engine/Checker.java
+++ src/main/java/ru/innopolis/university/summerbootcamp/java/project/engine/Checker.java
@@ -94,7 +94,7 @@ public class Checker {
             }
         }
         return -1000;
-    }
+    } // 10000
     private static int isStraightFlush(List<PlayingCard> cards) {
         for (int i = 0; i < 4; i++) {
             int count = 0;
@@ -112,14 +112,14 @@ public class Checker {
                     else {
                         count = 0;
                     }
-                    if (count == 5) {
-                        return j;
+                    if (count == 4) {
+                        return j+3;
                     }
                 }
             }
         }
         return -1000;
-    }
+    } // 9000+
     private static int isFourOfKind(List<PlayingCard> cards) {
         for (int j = 12; j >= 0; j--) {
             int counter = 0;
@@ -130,63 +130,63 @@ public class Checker {
                 return j;
         }
         return -1000;
-    }
+    } // 8000+
     private static int isFullHouse(List<PlayingCard> cards) {
-        int counter = 0;
         int sum = 1;
         int[] total = new int[13];
         for (int j = 12; j >= 0; j--)
         {
-            for(int i = 0; i < 4; i++)
-                if(pool[i][j])
-                    counter++;
-            if(counter == 2)
-                total[j] = 1;
-            if(counter > 2)
-                total[j] = 2;
+            int counter = 0;
+            for(int i = 0; i < 4; i++) {
+                if (pool[i][j]) { counter++;}
+            }
+            total[j] = counter;
+
         }
-        for(int j = 12; j >= 0; j--)
-            if(total[j] == 2)
-            {
-                sum *= j;
-                break;
-            }
-        for(int j = 12; j>= 0; j--)
-            if(total[j] >= 1 && j != sum) {
-                sum *= j;
-                break;
-            }
-        if(sum > 12)
-            return sum; // need to check it
+        int two = -1;
+        int three = -1;
+        for(int j = 12; j >= 0; j--) {
+            if (total[j] == 3 && j > three) { three = j;}
+        }
+        for(int j = 12; j >= 0; j--) {
+            if (total[j] >= 2 && j != three && j > three) { two = j;}
+        }
+        if(two != -1 && three != -1)
+            return((two+1)*(three+1));
         return -1000;
-    }
+    } // 7000+
     private static int isFlush(List<PlayingCard> cards) {
         for(int i = 0; i < 4; i++) {
             int counter = 0;
+            int highCard = -1;
             for (int j = 12; j >= 0; j--) {
                 if (pool[i][j]) {
+                    if(j > highCard) {highCard = j;}
                     counter++;
-                    if(counter == 4)
-                        return 0; // need to more work
+                    if(counter == 5)
+                        return highCard; // need to more work
                 }
             }
         }
         return -1000;
-    }
+    } // 6000+
     private static int isStraight(List<PlayingCard> cards) { // need to write some code for Ace correct working
-        for (int i = 0; i < 4; i++) {
-            int count = 0;
-            for (int j = 12; j > 0; j--)
-                if (pool[i][j] && pool[i][j - 1]) {
-                    count++;
-                    if (count == 5)
-                        return 0; // need to work more
-                }
-                else
-                    count = 0;
+        int count = 0;
+        boolean[] tmpPool = new boolean[13];
+        for(int j = 12; j >= 0; j--) {
+            for (int i = 0; i < 4; i++) {
+                if (pool[i][j])
+                    tmpPool[j] = true;
+            }
+        }
+        for (int j = 12; j > 0; j--) {
+            if (tmpPool[j] && tmpPool[j - 1]) {
+                count++;
+                if ((count == 4) || (count == 3 && tmpPool[12] && tmpPool[0])) return j+3; // need to work more
+            }
         }
         return -1000;
-    }
+    } // 5000+
     private static int isThreeOfKind(List<PlayingCard> cards) {
         for (int j = 12; j >= 0; j--) {
             int counter = 0;
@@ -194,25 +194,27 @@ public class Checker {
                 if (pool[i][j])
                     counter++;
             if(counter == 3)
-                return 12 - j;
+                return j;
         }
         return -1000;
-    }
+    } // 4000+
     private static int isTwoPairs(List<PlayingCard> cards) {
-        int counter = 0;
-        int sum = 0;
-
+        int pairs = 0;
+        int sum = 1;
         for (int j = 12; j >= 0; j--) {
+            int counter = 0;
             for (int i = 0; i < 4; i++)
                 if (pool[i][j])
                     counter++;
-            if(counter == 2)
-                sum++;
-            if(sum == 2)
-                return 0; // need more work with it
+            if(counter == 2) {
+                pairs++;
+                sum *= (j+1);
+            }
+            if(pairs == 2)
+                return sum; // need more work with it
         }
         return -1000;
-    }
+    } // 3000+
     private static int isOnePair(List<PlayingCard> cards) {
         for (int j = 12; j >= 0; j--) {
             int counter = 0;
@@ -220,15 +222,15 @@ public class Checker {
                 if (pool[i][j])
                     counter++;
             if(counter == 2)
-                return 12 - j;
+                return j;
         }
         return -1000;
-    }
+    } // 2000+
     private static int isHighCard(List<PlayingCard> cards) {
         PlayingCard card = cards.get(0);
         for(int i = 1; i<cards.size(); i++)
             if (cards.get(i).getRank() > card.getRank())
                 card = cards.get(i);
         return card.getRank();
-    }
+    } // 0+
 }
