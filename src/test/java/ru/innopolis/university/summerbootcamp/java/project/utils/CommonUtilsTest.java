ada17832e80bd8524c47561ab7871da9c7fdba83
Iskandar <iskandar@mbpro.local>
2016-07-19 17:31:42 +0300

    Tests: fix #HOLDEM-17

--- src/test/java/ru/innopolis/university/summerbootcamp/java/project/utils/CommonUtilsTest.java
+++ src/test/java/ru/innopolis/university/summerbootcamp/java/project/utils/CommonUtilsTest.java
@@ -12,27 +12,27 @@ public class CommonUtilsTest {
     public void inRangeTest() {
         Assert.assertTrue(
                 "InRange function fails on integers (in range)",
-                CommonUtils.isInRange(new Integer(1), new Integer(10), new Integer(20))
+                CommonUtils.isIn(new Integer(1), new Integer(10), new Integer(20))
         );
 
         Assert.assertTrue(
                 "InRange function fails on integers (not in range)",
-                !CommonUtils.isInRange(new Integer(1), new Integer(21), new Integer(20))
+                !CommonUtils.isIn(new Integer(1), new Integer(21), new Integer(20))
         );
 
         Assert.assertTrue(
                 "InRange function fails on integers (in range, extreme value)",
-                CommonUtils.isInRange(new Integer(1), new Integer(20), new Integer(20))
+                CommonUtils.isIn(new Integer(1), new Integer(20), new Integer(20))
         );
 
         Assert.assertTrue(
                 "InRange function fails on double (int range, extreme value)",
-                CommonUtils.isInRange(new Double(-1), new Double(19.9999), new Double(20))
+                CommonUtils.isIn(new Double(-1), new Double(19.9999), new Double(20))
         );
 
         Assert.assertTrue(
                 "InRange function fails on double (not in range, extreme value)",
-                !CommonUtils.isInRange(new Double(1), new Double(20.1), new Double(20))
+                !CommonUtils.isIn(new Double(1), new Double(20.1), new Double(20))
         );
     }
 }
